621c7081497083c03dbe7087959ac991ec1f4331
Ekaterina Zhdanova <katherine.inspiration@gmail.com>
2020-06-19 11:54:17 +0300

    Fixed body-parser using

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 var pgClient = require('./postgreClient');
 
 var app = express();
 const router = express.Router();
 
-
-
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(logger('dev'));
 app.use(express.json());
