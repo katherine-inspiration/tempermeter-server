e3db8b1dff711af53eecb138131bda89b1524c65
Ekaterina Zhdanova <katherine.inspiration@gmail.com>
2020-08-05 02:25:29 +0300

    trying to get something from ok

--- app.js
+++ app.js
@@ -15,124 +15,126 @@ const port = process.env.PORT || 3000;
 
 var ok = require('ok.ru');
 let requestOptions = {
-  applicationSecretKey: process.env.OK_APPLICATION_SECRET_KEY,
-  applicationKey: process.env.OK_APPLICATION_KEY,
-  applicationId: process.env.OK_APPLICATION_ID,
-  accessToken: process.env.OK_ACCESS_TOKEN
+    applicationSecretKey: process.env.OK_APPLICATION_SECRET_KEY,
+    applicationKey: process.env.OK_APPLICATION_KEY,
+    applicationId: process.env.OK_APPLICATION_ID,
+    accessToken: process.env.OK_ACCESS_TOKEN
 };
 ok.setOptions(requestOptions);
+new ok.api('get', {method: 'users.isAppUser'}, (err, data, response) => {
+    //some actions with data
+    alert("Alarm! Error! #{err}");
+    if (err)
+        //It can transport all response and headers
+        console.log(response)
+});
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-
-
-
 app.get('/api/answer_ball/:answer_id', (req, res) => {
-  pgClient.sendAnswerBall(req.params.answer_id, res);
+    pgClient.sendAnswerBall(req.params.answer_id, res);
 });
 
 app.get('/api/answers/:question_id', (req, res) => {
-  pgClient.sendAnswers(req.params.question_id, res);
+    pgClient.sendAnswers(req.params.question_id, res);
 });
 
 app.get('/api/history/results/:user_id', (req, res) => {
-  pgClient.sendResultsHistory(req.params.user_id, res);
+    pgClient.sendResultsHistory(req.params.user_id, res);
 });
 
 app.get('/api/history/answers/:session_id', (req, res) => {
-  pgClient.sendAnswersHistory(req.params.session_id, res);
+    pgClient.sendAnswersHistory(req.params.session_id, res);
 });
 
 app.get('/api/history/answers/:session_id/:question_id', (req, res) => {
-  pgClient.sendAnswer(req.params.session_id, req.params.question_id, res);
+    pgClient.sendAnswer(req.params.session_id, req.params.question_id, res);
 });
 
 app.get('/api/images/holeric', (req, res) => {
-  res.sendFile(path.join(__dirname, 'images', 'holeric.PNG'));
+    res.sendFile(path.join(__dirname, 'images', 'holeric.PNG'));
 });
 
 app.get('/api/images/flegmatic', (req, res) => {
-  res.sendFile(path.join(__dirname, 'images', 'flegmatic.png'));
+    res.sendFile(path.join(__dirname, 'images', 'flegmatic.png'));
 });
 
 app.get('/api/images/sangvinic', (req, res) => {
-  res.sendFile(path.join(__dirname, 'images', 'sangvinic.PNG'));
+    res.sendFile(path.join(__dirname, 'images', 'sangvinic.PNG'));
 });
 
 app.get('/api/images/melanholic', (req, res) => {
-  res.sendFile(path.join(__dirname, 'images', 'melanholic.PNG'));
+    res.sendFile(path.join(__dirname, 'images', 'melanholic.PNG'));
 });
 
 app.get('/api/questions', (req, res) => {
-  pgClient.sendQuestions(res);
+    pgClient.sendQuestions(res);
 });
 
 app.get('/api/result/calculate/:user_id/:session_id', (req, res) => {
-  pgClient.calculateResult(req.params.user_id, req.params.session_id, res);
+    pgClient.calculateResult(req.params.user_id, req.params.session_id, res);
 });
 
 app.get('/api/session/:user_id', (req, res) => {
-  pgClient.sendCurrentSessionId(req.params.user_id, res);
+    pgClient.sendCurrentSessionId(req.params.user_id, res);
 });
 
 app.get('/api/session/:session_id/finish', (req, res) => {
-  pgClient.finishSession(req.params.session_id, res);
+    pgClient.finishSession(req.params.session_id, res);
 });
 
 app.get('/api/session/start/:user_id', (req, res) => {
-  pgClient.startSession(req.params.user_id, res);
+    pgClient.startSession(req.params.user_id, res);
 });
 
 app.get('/api/sessions/:user_id', (req, res) => {
-  pgClient.sendFinishedSessions(req.params.user_id, res);
+    pgClient.sendFinishedSessions(req.params.user_id, res);
 });
 
 app.get('/api/result/:session_id', (req, res) => {
-  console.log("Sending a result of the session " + req.params.session_id);
-  pgClient.sendResultData(req.params.session_id, res);
+    console.log("Sending a result of the session " + req.params.session_id);
+    pgClient.sendResultData(req.params.session_id, res);
 });
 
 
 app.put('/api/answer', (req, res) => {
-  pgClient.putAnswer(req.body, res);
+    pgClient.putAnswer(req.body, res);
 });
 
 app.put('/api/result', (req, res) => {
-  pgClient.putResult(req.body, res);
+    pgClient.putResult(req.body, res);
 });
 
 
-
 app.get('/api', (req, res) => {
-  res.json({text: "Hello, api!"})
+    res.json({text: "Hello, api!"})
 });
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
