81aa1e1826bf2f5d9cdb48bccd2b8338cff70eb6
Ekaterina Zhdanova <katherine.inspiration@gmail.com>
2020-08-31 09:59:17 +0300

    Fixed userId in FinishConfirmWindow

---
+++ build/precache-manifest.3001d70c57656d9f258c621548f9bd22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847d0376bf569614500e1d54126e0bdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a39cd53042505131dcee",
+    "url": "/static/css/main.2cbae279.chunk.css"
+  },
+  {
+    "revision": "a9a419f1ddba8087ea9d",
+    "url": "/static/js/2.e94a452a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e94a452a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39cd53042505131dcee",
+    "url": "/static/js/main.675afd40.chunk.js"
+  },
+  {
+    "revision": "bc85d868fe33ac4a7221",
+    "url": "/static/js/runtime-main.2600cc24.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61e02c6ad8123197aa8e15c1f2432acf.js"
+  "/precache-manifest.3001d70c57656d9f258c621548f9bd22.js"
 );
 
 self.addEventListener('message', (event) => {
