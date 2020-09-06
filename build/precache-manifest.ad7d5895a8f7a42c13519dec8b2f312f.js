dc6cefe2987990ffec45a4da870167aadf68d72b
Ekaterina Zhdanova <katherine.inspiration@gmail.com>
2020-09-06 17:32:41 +0300

    changed header layout

---
+++ build/precache-manifest.ad7d5895a8f7a42c13519dec8b2f312f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78e974e8b2929967c51b7c419aa837a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ecfd0bbbbca9612ba26",
+    "url": "/static/css/main.2cbae279.chunk.css"
+  },
+  {
+    "revision": "d27344811aeae3f505e3",
+    "url": "/static/js/2.4a923300.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.4a923300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ecfd0bbbbca9612ba26",
+    "url": "/static/js/main.549cf18a.chunk.js"
+  },
+  {
+    "revision": "bc85d868fe33ac4a7221",
+    "url": "/static/js/runtime-main.2600cc24.js"
+  }
+]);
